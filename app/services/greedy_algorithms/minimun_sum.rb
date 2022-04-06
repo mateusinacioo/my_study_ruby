66546a2c3fb8e3e356a010c6600d3cc8f3d1f940
mateusinacioo <mateus.inacio321@gmail.com>
2022-04-06 15:22:03 -0300

    Feito exercicio Minimun Sumruby /home/mateus/Documentos/my_study_ruby/app/services/greedy_algorithms/minimun_sum.rb

---
+++ app/services/greedy_algorithms/minimun_sum.rb
@@ -0,0 +1,15 @@
+def minSum(a)
+
+	# find the minimum element of A[]
+	min_val = a.min
+    # 2
+    len = a.length
+    # 7
+	
+	result = min_val * (len-1)
+                 
+
+end
+# driver code
+a = [2, 4, 5, 3, 4, 9]
+p minSum(a)
