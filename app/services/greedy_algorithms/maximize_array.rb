492e67ad7d9b7c5db0469866f83e92cfa468d3ee
mateusinacioo <mateus.inacio321@gmail.com>
2022-04-07 15:31:17 -0300

    Resolvendo exercicio

--- app/services/greedy_algorithms/maximize_array.rb
+++ app/services/greedy_algorithms/maximize_array.rb
@@ -1,10 +1,15 @@
 def maximizeArray (lista)
     len = lista.length
-        for i in 0..len
-        p "#{i} #{lista[i].abs()}"
+    for i in 0..len
+        if lista[i] < 0
+            aux = (lista[i] *(-1))
+        else
+            soma = lista[i]
+        end
     end
-    lista
+    soma
 end
 
 arr = [-2, 0, 5, -1, 2]
+soma= 0
 p maximizeArray(arr)
